db9a31754a20b170f0290b843cab3d1266489afe
Samit Sarkar <samsarka@microsoft.com>
2019-06-21 13:02:20 +0530

    Add project files.

---
+++ AzureStorageCopydatafromExternalresources/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.WindowsAzure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Blob; // Namespace for Blob storage types
+using Microsoft.WindowsAzure.Storage.Auth;
+using System;
+using System.IO;
+
+namespace AzureStorageCopydatafromExternalresources
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CopydatafromExternalresources();
+            Console.WriteLine("Copied..");
+            Console.ReadLine();
+
+        }
+
+
+        static void CopydatafromExternalresources()
+        {
+            string accountName = "";
+            string accountKey = "";
+            string newFileName = "";
+            string destinationContainer = "";
+            string sourceUrl = "";
+
+            CloudStorageAccount csa = new CloudStorageAccount(new StorageCredentials(accountName, accountKey), true);
+            CloudBlobClient blobClient = csa.CreateCloudBlobClient();
+            var blobContainer = blobClient.GetContainerReference(destinationContainer);
+            blobContainer.CreateIfNotExists();
+            var newBlockBlob = blobContainer.GetBlockBlobReference(newFileName);
+            newBlockBlob.StartCopy(new Uri(sourceUrl), null, null, null);
+        }
+    }
+}
